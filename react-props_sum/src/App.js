4698417c21d6acb3a2af45713ea4b191ec3e347b
Jonas Dally <jonas.dall@hotmail.de>
2023-07-18 15:33:04 +0200

    adding single values

--- react-props_sum/src/App.js
+++ react-props_sum/src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <Sum valueA={1} valueB={2} />;
+  return <Sum valueA={3} valueB={2} />;
 }
 
 function Sum({ valueA, valueB }) {
-  return <h1>The Sum is: {valueA + valueB}</h1>;
+  return (
+    <h1>
+      The Sum is: {valueA} + {valueB} = {valueA + valueB}
+    </h1>
+  );
 }
