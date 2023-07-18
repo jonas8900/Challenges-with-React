0295ec6a8c9e3465a93f2c1a95cc1c019abf6eca
Jonas Dally <jonas.dall@hotmail.de>
2023-07-18 15:29:54 +0200

    add second task props

---
+++ react-props_sum/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <Sum valueA={1} valueB={2} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return <h1>The Sum is: {valueA + valueB}</h1>;
+}
